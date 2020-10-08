4c96b68a446f67906d30350585ebaeba374eed2e
Vishnu Anand <vishnuanand2000@gmail.com>
2020-10-08 16:34:28 +0530

    Implemented data binding on recycler view

--- app/src/main/java/com/vishnu/todoapp/fragments/BindindAdapters.kt
+++ app/src/main/java/com/vishnu/todoapp/fragments/BindindAdapters.kt
@@ -3,12 +3,16 @@ package com.vishnu.todoapp.fragments
 import android.renderscript.RenderScript
 import android.view.View
 import android.widget.Spinner
+import androidx.cardview.widget.CardView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.BindingAdapter
 import androidx.lifecycle.MutableLiveData
 import androidx.navigation.findNavController
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.vishnu.todoapp.R
 import com.vishnu.todoapp.data.models.Priority
+import com.vishnu.todoapp.data.models.ToDoData
+import com.vishnu.todoapp.fragments.list.ListFragmentDirections
 
 class BindindAdapters {
 
@@ -42,6 +46,23 @@ class BindindAdapters {
                 Priority.MEDIUM -> { view.setSelection(1)}
                 Priority.LOW -> { view.setSelection(2)}
             }
+        }
+
+        @BindingAdapter("android:parsePriorityColour")
+        @JvmStatic
+        fun parsePriorityColour(cardView: CardView, priority: Priority){
+            when(priority) {
+                Priority.HIGH -> { cardView.setCardBackgroundColor(cardView.context.getColor(R.color.red))}
+                Priority.MEDIUM -> { cardView.setCardBackgroundColor(cardView.context.getColor(R.color.yellow))}
+                Priority.LOW -> { cardView.setCardBackgroundColor(cardView.context.getColor(R.color.green))}
+            }
+        }
+
+        @BindingAdapter("android:sendDataToUpdateFragment")
+        @JvmStatic
+        fun sendDataToUpdateFragment(view: ConstraintLayout, currentItem: ToDoData) {
+            val action = ListFragmentDirections.actionListFragmentToUpdateFragment(currentItem)
+            view.findNavController().navigate(action)
         }
     }
 }
